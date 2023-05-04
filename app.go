2f29ffafdd8810840360d54687ee70f0c630b7c2
RudyWang <a86742902@gmail.com>
2023-05-04 16:58:04 +0800

    AAAAA

--- app.go
+++ app.go
@@ -13,6 +13,7 @@ func main() {
 	bot, _ := linebot.New("06ba3775aca06dc01fcf536baf3145c7",
 		"/ppMUopJaNfW1sbqXSAd8fncob/p+yfD1r37r5P3SkMA0LHWUfjycs2c9gyykwpmLlFa8cKgF8cZfrZYZZxlQawzj43YNZd5IPWNx31WI42s6is5b9aCvBASgNT+Jt7z3X+ORJuSEvsPPlPlXymc9AdB04t89/1O/w1cDnyilFU=")
 	var timestamp time.Time
+	var newTimestamp time.Time
 	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
 		events, err := bot.ParseRequest(req)
 		if err != nil {
@@ -34,19 +35,14 @@ func main() {
 						// 轉換為UTC+8時間
 						timestamp = event.Timestamp.In(loc)
 
-						newTimestamp := timestamp.Add(time.Minute * 10)
-						newTimestamp = newTimestamp.Add(time.Hour * 9)
-
-						msg := "上崗時間：" + timestamp.Format("15:04:05") + "\n" + "下崗時間" + newTimestamp.Format("15:04:05")
-
-						if _, err = bot.ReplyMessage(event.ReplyToken,
-							linebot.NewTextMessage(msg)).Do(); err != nil {
-							log.Print(err)
-						}
-						return
 					}
+					newTimestamp = timestamp.Add(time.Minute * 10)
+					newTimestamp = newTimestamp.Add(time.Hour * 9)
+
+					msg := "上崗時間：" + timestamp.Format("15:04:05") + "\n" + "下崗時間：" + newTimestamp.Format("15:04:05")
+
 					if _, err = bot.ReplyMessage(event.ReplyToken,
-						linebot.NewTextMessage("????")).Do(); err != nil {
+						linebot.NewTextMessage(msg)).Do(); err != nil {
 						log.Print(err)
 					}
 				}
